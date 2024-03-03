c7f8e5e5003c5aabfb008c11716a4f20e10039ad
anaserrami <anaserrami24@gmail.com>
2024-03-03 23:21:59 +0100

    second activity

---
+++ Activité Pratique N°2 - ORM_JPA_Hibernate Spring Data/src/main/java/ma/enset/hospitalapp/HospitalAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.hospitalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppApplication.class, args);
+	}
+
+}
---
+++ Activité Pratique N°2 - ORM_JPA_Hibernate Spring Data/src/main/java/ma/enset/hospitalapp/entities/Medecin.java
@@ -0,0 +1,4 @@
+package ma.enset.hospitalapp.entities;
+
+public class Medecin {
+}
